e0f72f615daf00c02af3b041cc38bd364dad7c5c
Likhon22 <likhonsarker793@gmail.com>
2025-11-02 23:38:25 +0600

    update: usercustomer validation updated

--- user_service/src/internal/api/handlers/userCustomer/userCustomer.validation.ts
+++ user_service/src/internal/api/handlers/userCustomer/userCustomer.validation.ts
@@ -5,7 +5,7 @@ export const TCustomerSchema = z.object({
     name: z.string().min(1, 'Name is required'),
     phone: z.string().optional(),
     address: z.string().optional(),
-    avatarUrl: z.string().url().optional(),
+    avatarUrl: z.string().optional(),
     email: z.string().min(1, 'Email is required').email('Invalid email type'),
     password: z.string().min(6, 'Password must be at least 6 characters'),
   }),
